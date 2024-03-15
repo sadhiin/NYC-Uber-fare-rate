0f99a56b7e51c61b005a12345f1947643329b259
Shanjidul Islam Sadhin <sanjidulislamsadhin1994@gmail.com>
2024-03-15 19:19:15 +0000

    initial project sructure is created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project aims to predict fare rates for Uber transactions in New York City. By leveraging historical trip data, we intend to develop a model that accurately estimates fare prices for future rides.',
+    author='sadhin',
+    license='MIT',
+)
